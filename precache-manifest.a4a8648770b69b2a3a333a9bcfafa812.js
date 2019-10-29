253450446b7e2067a8035b91033c81f9ca79972d
Viktoriia <jhunt@ukr.net>
2019-10-29 12:59:41 +0200

    Updates

---
+++ precache-manifest.a4a8648770b69b2a3a333a9bcfafa812.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4d6cea36fc8c9e718aacf7657f83ab",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "/react_static-list-of-todos/static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "ad1d20e23d739ebfdfc3",
+    "url": "/react_static-list-of-todos/static/js/main.a637799b.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
